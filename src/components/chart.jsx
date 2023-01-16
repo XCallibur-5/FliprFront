96bf1ea8d3d70c69d84cb92310e31d1d4d6f1887
unknown <you@example.com>
2023-01-16 20:17:00 +0530

    cosmetic change.

--- src/components/chart.jsx
+++ src/components/chart.jsx
@@ -6,7 +6,6 @@ import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
-import Button from '@mui/material/Button';
 
 import {
   Chart as ChartJS,
