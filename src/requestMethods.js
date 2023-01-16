b3d6c089fe097d2936d75b7bfae43c34762d511c
unknown <you@example.com>
2023-01-16 21:06:00 +0530

    request made to deployed server.

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://127.0.0.1:8000/";
+const BASE_URL = "https://stockapi.truebuddyz.com/";
 
 const user =  JSON.parse(localStorage.getItem('authorized'));
 
